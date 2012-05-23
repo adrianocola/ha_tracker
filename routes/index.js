2227a062306d5d414a78efd2cae7aee15641bf77
Adriano Cola <adrianocola@gmail.com>
2012-05-23 16:16:59 -0300

    Added footer with like buttons, sharing tools and references to libraries

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,10 @@ app.get('/', function(req, res){
     //If the session is active or the user is with KEEP_LOGGED_IN cookies,
     //notify the client that he must try to login without credentials
     if(req.session.userId || (req.cookies.KEEP_LOGGED_USER && req.cookies.KEEP_LOGGED_ID)){
-        res.render('index', {data: { title: "HA Tracker", salt: env.salt, facebook_app_id: env.facebook_app_id, is_logged: true}});
+        res.render('index', {data: { salt: env.salt, facebook_app_id: env.facebook_app_id, is_logged: true}});
 
     }else{
-        res.render('index', {data: { title: "HA Tracker", salt: env.salt, facebook_app_id: env.facebook_app_id }});
+        res.render('index', {data: { salt: env.salt, facebook_app_id: env.facebook_app_id }});
     }
 
 
@@ -24,11 +24,18 @@ app.get('/enemies/:enemyId/:gameId', function(req, res){
     //If the session is active or the user is with KEEP_LOGGED_IN cookies,
     //notify the client that he must try to login without credentials
     if(req.session.userId || (req.cookies.KEEP_LOGGED_USER && req.cookies.KEEP_LOGGED_ID)){
-        res.render('index', {data: { title: "HA Tracker", salt: env.salt, facebook_app_id: env.facebook_app_id, is_logged: true}});
+        res.render('index', {data: { salt: env.salt, facebook_app_id: env.facebook_app_id, is_logged: true}});
 
     }else{
-        res.render('index', {data: { title: "HA Tracker", salt: env.salt, facebook_app_id: env.facebook_app_id }});
+        res.render('index', {data: { salt: env.salt, facebook_app_id: env.facebook_app_id }});
     }
 
+
+});
+
+
+app.get('/terms_privacy', function(req, res){
+
+    res.render('terms_privacy', {data: { salt: env.salt, facebook_app_id: env.facebook_app_id }});
 
 });
